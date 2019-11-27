a65dd8517db917646cd0edfda25e8c6df44f9599
Nate Glasgow <ngglasgow@gmail.com>
2019-11-27 16:15:23 -0500

    refactored events into class

--- clamp_ephys/events.py
+++ clamp_ephys/events.py
@@ -6,6 +6,66 @@ import matplotlib.pyplot as plt
 from neo.io import IgorIO
 import os
 
+class Trial:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+    def find_events(self, start_threshold, end_threshold):
+        self.xs = []
+        self.ys = []
+        self.durs = []
+
+        starts = np.where(self.y > start_threshold, self.y, 0)
+        stops = np.where(self.y > end_threshold, self.y, 0)
+
+        start_indices = np.nonzero(np.diff(np.sign(starts)) == 1)[0] + 1
+        stop_indices = np.nonzero(np.diff(np.sign(stops)) == -1)[0]
+
+        self.num_events = len(stop_indices)
+
+        for event in range(self.num_events):
+            event_x = self.x[start_indices[event]:stop_indices[event] + 1]
+            event_y = self.y[start_indices[event]:stop_indices[event] + 1]
+            duration = len(event_x) / 10
+
+            self.xs.append(event_x)
+            self.ys.append(event_y)
+            self.durs.append(duration)
+    
+    def events_in_window(self, window_start, window_stop, duration_threshold):
+        self.is_event_trial = []
+        for event in range(self.num_events):
+            event_x = self.xs[event]
+            event_y = self.ys[event]
+            duration = self.durs[event]
+
+            in_window = np.any(
+                event_x[(event_x > window_start) & (event_x < window_stop)]
+            )
+
+        if in_window == True:
+            if duration >= duration_threshold:
+                is_event = 1
+                self.is_event_trial.append(is_event)
+            else:
+                is_event = 0
+                self.is_event_trial.append(is_event)
+        else:
+            is_event = 0
+            self.is_event_trial.append(is_event)
+
+    def any_event_in_window(self):
+        if sum(self.is_event_trial) >= 1:
+            event_trial = 1
+
+        elif sum(self.is_event_trial) == 0:
+            event_trial = 0
+    
+        return event_trial
+
+
 
 x = np.linspace(0, 100, 10000)
 y = np.sin(x)
@@ -21,7 +81,7 @@ stop_indices = np.nonzero(np.diff(np.sign(stops)) == -1)[0]
 
 num_events = len(stop_indices)
 
-xs, ys, durations = [], [], []
+xs, ys, durs = [], [], []
 
 for event in range(num_events):
     event_x = x[start_indices[event]:stop_indices[event] + 1]
