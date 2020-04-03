b8bc9c3478804f57f3a431a53fe28343ccf5d6a2
Nathan Glasgow <ngglasgow@gmail.com>
2020-04-02 21:18:27 -0400

    cleaned up files

--- archive/response_checker_jane.py
+++ archive/response_checker_jane.py
@@ -8,240 +8,41 @@ epsc_peak_x.y.z.py
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style(rc = {'figure.facecolor':'white'})
 import elephant
 from neo.io import IgorIO
 import os
 import platform
+from collections import OrderedDict
+import math
 
+paths = file_structure('local', 'Injected_GC_data/VC_pairs')
 
-def igor_to_pandas(file, data_dir):
-    '''This function opens an igor binary file (.ibw), extracts the time
-    series data, and returns a pandas DataFrame'''
+# analysis for p2 data set
+# Parameters (sampling frequency = 25 kHz, data collected as pA, test pulse is +10 mV jump
+#                  and starts at t = 5 ms)
+ 
+file_name_list, data_notes = create_data_notes('p2', 'p2_MC_TC_summary.csv', paths.p2_files)
+responses_df = cell_response_checker('p2', file_name_list, paths.p2, sf=25)
 
-    file_path = os.path.join(data_dir, file)
-    data_raw = IgorIO(filename=file_path)
-    data_neo = data_raw.read_block()
-    data_neo_array = data_neo.segments[0].analogsignals[0]
-    data_df = pd.DataFrame(data_neo_array.as_array())
-
-    return data_df
-
-
-def mean_baseline(data, stim_time, pre_stim=100, sf=10):
-    '''
-    Find the mean baseline in a given time series
-    Parameters
-    ----------
-    data: pandas.Series or pandas.DataFrame
-        The time series data for which you want a baseline.
-    stim_time: int or float
-        The time in ms when stimulus is triggered.
-    pre_stim: int or float
-        Time in ms before the stimulus trigger over which baseline is measured.
-    sf: int or float
-        The sampling frequency in kHz.
-
-    Returns
-    -------
-    baseline: float or pandas.Series
-        The mean baseline over the defined window
-    '''
-    start = (stim_time - pre_stim) * sf
-    stop = (stim_time - 1) * sf
-    window = data.iloc[start:stop]
-    baseline = window.mean()
-
-    return baseline
-
-
-def std_baseline(data, stim_time, pre_stim=100, sf=10):
-    '''
-    Find the baseline st. dev. in a given time series
-    Parameters
-    ----------
-    data: pandas.Series or pandas.DataFrame
-        The time series data for which you want a baseline.
-    stim_time: int or float
-        The time in ms when stimulus is triggered.
-    pre_stim: int or float
-        Time in ms before the stimulus trigger over which baseline is measured.
-    sf: int or float
-        The sampling frequency in kHz.
-
-    Returns
-    -------
-    std: float or pandas.Series
-        The baseline st. dev. over the defined window
-    '''
-    start = (stim_time - pre_stim) * sf
-    stop = (stim_time - 1) * sf
-    window = data.iloc[start:stop]
-    std = window.std()
-
-    return std
-
-
-def epsc_peak(data, baseline, stim_time, polarity='-', post_stim=50, sf=10):
-    '''
-    Find the peak EPSC value for a pandas.Series or for each sweep (column) of
-    a pandas.DataFrame. This finds the absolute peak value of mean baseline
-    subtracted data.
-
-    Parameters:
-    -----------
-    data: pandas.Series or pandas.DataFrame
-        Time series data with stimulated synaptic response triggered at the
-        same time for each sweep.
-    baseline: scalar or pandas.Series
-        Mean baseline values used to subtract for each sweep.
-    stim_time: int or float
-        Time in ms at which stimulus is triggered each sweep.
-    polarity: str
-        The expected polarity of the EPSC; negative: '-'; postitive: '+'.
-        Default is '-'.
-    post_stim: int or float
-        Time in ms that marks the end of the sampling window post stimulus.
-        Default is 100 ms.
-    sf: int or float
-        The sampling frequency in kHz. Default is 10 kHz.
-
-    Returns
-    -------
-    epsc_peaks: pandas.Series
-        The absolute peak of mean baseline subtracted time series data.
-    '''
-
-    subtracted_data = data - baseline
-    start = stim_time * sf
-    end = (stim_time + post_stim) * sf
-    peak_window = subtracted_data.iloc[start:end]
-
-    if polarity == '-':
-        epsc_peaks = peak_window.min()
-    elif polarity == '+':
-        epsc_peaks = peak_window.max()
-    else:
-        raise ValueError(
-            "polarity must either be + or -"
-        )
-
-    return epsc_peaks
-
-
-''' *********************************************************************** '''
-
-''' ################## Define file structure on server #################### '''
-# home_dir will depend on the OS, but the rest will not
-# query machine identity and set home_dir from there
-machine = platform.uname()[0]
-
-if machine == 'Darwin':
-    home_dir = '/Volumes/Urban'
-
-elif machine == 'Linux':
-    home_dir = '/run/user/1000/gvfs/smb-share:server=130.49.237.41,share=urban'
-
-elif machine == 'Windows':
-    home_dir = r"N:\urban\Huang"
-
-else:
-    print("OS not recognized. \nPlease see Nate for correction.")
-
-project_dir = os.path.join(home_dir, 'Injected_GC_data', 'New_VC_pairs')
-figure_dir = os.path.join(project_dir, 'figures')
-table_dir = os.path.join(project_dir, 'tables')
-data_dir = os.path.join(project_dir, 'data')
-
-''' ## Open the notes spreadsheet and parse for what we want to analyze ## '''
-# open metadata file
-allcells_data_notes = pd.read_csv(os.path.join(table_dir, 'MC_TC_summary.csv'))
-
-# pull out cell_id for directory, file name, and make the full path
-file_name_list = allcells_data_notes['Cell name'].tolist()
-cell_id_list = []
-
-for file in file_name_list:
-    file_split = file.split('_')
-    cell_id = file_split[0]+'_'+file_split[1]
-    cell_id_list.append(cell_id)
-
-file_path_list = []
-
-for cell, file in zip(cell_id_list, file_name_list):
-    file_path = os.path.join(file + '.ibw')
-    file_path_list.append(file_path)
-
-allcells_data_notes = pd.concat([pd.DataFrame({'File Path': file_path_list}), 
-    allcells_data_notes], axis=1)
-light_data_notes = allcells_data_notes[allcells_data_notes['Cell name'].str.contains("light")]
-spontaneous_data_notes = allcells_data_notes[allcells_data_notes['Cell name'].str.contains("spontaneous")]
-
-allcells_data_notes.to_csv(os.path.join(table_dir, 'allcells_data_notes.csv'))
-light_data_notes.to_csv(os.path.join(table_dir, 'light_data_notes.csv'))
-spontaneous_data_notes.to_csv(os.path.join(table_dir, 'spontaneous_data_notes.csv'))
-
-''' ##########################################################################
-This is all the analysis, figures, saving
-Read in file metadata, open file from igor, convert to pandas
-##############################################################################
-'''
-# make empty lists for response answers for each file
-responses_df = pd.DataFrame()
-
-# loop through all the files in file_name_list for plots and saving
-for file_name, file_path in zip(file_name_list, file_path_list):
-    # set file name from list
-    file = file_name
-
-    # open igor file and convert to pandas
-    data = igor_to_pandas(file_path, data_dir)
-
-    '''
-        Pull out EPSC peak from unfiltered signals
-        Baseline 100 ms preceding blue light
-        Peak within 250 ms of blue light
-    '''
-
-    baseline = mean_baseline(data, 500)
-    peaks = epsc_peak(data, baseline, 500)
-
-    '''
-        Pull out EPSC peaks from filtered signals
-        Baseline 100 ms preceding blue light
-        Peak within 250 ms of blue light
-    '''
-
-    # filter signal with butterworth filter at 500 Hz for data
-    filt_data = elephant.signal_processing.butter(data.T,
-                                                  lowpass_freq=500.0,
-                                                  fs=10000.0)
-    filt_data = pd.DataFrame(filt_data).T
-
-    filt_baseline = mean_baseline(filt_data, 500)
-    filt_baseline_std = std_baseline(filt_data, 500)
-    filt_peaks = epsc_peak(filt_data, filt_baseline, 500)
-
-    # make binary choice of whether the response is more than 3x std
-    mean_std = filt_baseline_std.mean()
-    mean_peaks = filt_peaks.mean()
-
-    response_2x = abs(mean_peaks) > mean_std * 2
-    response_3x = abs(mean_peaks) > mean_std * 3
-
-    responses = pd.DataFrame({'Cell name': file_name,
-                              'Mean Peaks (nA)': mean_peaks,
-                              'Mean STD (nA)': mean_std,
-                              'Response 2x STD': response_2x,
-                              'Response 3x STD': response_3x,
-                              },
-                              index=range(1))
-
-    responses_df = pd.concat([responses_df, responses], ignore_index=True)
-
-# save responses as csv
-responses_df.to_csv(os.path.join(table_dir, 'responses.csv'))
 
 # merge with data_notes and save
-responses_data_notes = pd.merge(allcells_data_notes, responses_df)
-responses_data_notes.to_csv(os.path.join(table_dir, 'responses_data_notes.csv'),
+responses_data_notes = pd.merge(data_notes, responses_df)
+responses_data_notes.to_csv(os.path.join(paths.tables, 'p2_responses_data_notes.csv'),
+                            float_format='%8.4f', index=False)
+
+
+# analysis for p14 data set
+# Parameters (sampling frequency = 10 kHz, data collected as nA, test pulse is -5 mV jump
+#                  and starts at t = 50 ms)
+ 
+file_name_list, data_notes = create_data_notes('p14', 'p14_MC_TC_summary.csv', paths.p14_files)
+responses_df = cell_response_checker('14', file_name_list, paths.p14, sf=10)
+
+
+# merge with data_notes and save
+responses_data_notes = pd.merge(data_notes, responses_df)
+responses_data_notes.to_csv(os.path.join(paths.tables, 'p14_responses_data_notes.csv'),
                             float_format='%8.4f', index=False)
