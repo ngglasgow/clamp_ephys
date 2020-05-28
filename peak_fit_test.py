61dd1b6ed29b383482eabe851921ee091291766c
Jane Huang <jhuang@pop-os.localdomain>
2020-05-28 17:28:29 -0400

    tested saving / kinetics df on peak_fit_test

--- peak_fit_test.py
+++ peak_fit_test.py
@@ -23,8 +23,7 @@ width = 3
 # paths to clamp_ephys/test_data/
 project_path = os.path.join(os.path.expanduser('~'), 'Documents', 'phd_projects', 'Injected_GC_data')
 notes_path = os.path.join(project_path, 'VC_pairs', 'tables', 'p2_data_notes.csv')
-cell_path = os.path.join(project_path, 'VC_pairs', 'data', 'p2', 'JH200313_c3_light100.ibw')
-sweep = 9
+cell_path = os.path.join(project_path, 'VC_pairs', 'data', 'p2', 'JH200313_c2_spontaneous_depol.ibw')
 
 data = clamp_ephys.workflows.cell(cell_path, fs=fs, path_to_data_notes=notes_path, timepoint='p2', amp_factor=amp_factor)
 
@@ -32,8 +31,86 @@ data.get_raw_peaks(stim_time, post_stim, polarity='-', baseline_start=baseline_s
 data.filter_traces(lowpass_freq)
 data.get_filtered_peaks(stim_time, post_stim)
 
-sweeps = data.traces - data.new_baseline_raw
+data.get_peaks_widths(stim_time, fs, width)   
+
+sweeps = data.traces_filtered - data.new_baseline_raw
+
 window_start = (stim_time + 20) * fs
+all_peaks_kinetics_df = pd.DataFrame()
+all_peaks_kinetics_avg_df = pd.DataFrame()
+first3_kinetics_avg_df = pd.DataFrame()
+
+for sweep in range(len(sweeps.columns)):        
+    trace = sweeps.iloc[window_start:, sweep].values
+
+
+    ninety_left = data.all_widths_df.loc[(sweep), 'ninety_left']
+    ten_left = data.all_widths_df.loc[(sweep), 'ten_left']
+    ten_to_ninety = (ninety_left - ten_left) / fs
+    peak_time = data.all_widths_df.loc[(sweep), 'peaks_index'] / fs
+    hw_time = data.all_widths_df.loc[(sweep), 'half_widths'] / fs
+    
+    peak_numbers = range(len(data.all_widths_df.loc[sweep]))
+
+    tau_list = []
+    charge_list = []
+
+    for peak_number in peak_numbers:        
+        tau = data.get_tau(trace, sweep, peak_number)
+        tau_list.append(tau)
+        charge = data.get_charge_transf(trace, sweep, peak_number)
+        charge_list.append(charge) 
+    
+    all_peaks_kinetics_data = pd.DataFrame({'sweep #': sweep, 'peak #': peak_numbers, 'peak time (ms)': peak_time, '10 to 90% RT (ms)': ten_to_ninety,
+        'tau': tau_list, 'half-width (ms)': hw_time, 'charge transferred (pA * s)': charge_list})
+    all_peaks_kinetics_data = all_peaks_kinetics_data[all_peaks_kinetics_data.tau < 500] # drops peaks with tau values over 50
+
+    if len(all_peaks_kinetics_data) == 0: # moves onto the next sweep if no peaks exist after dropping tau values
+        continue
+
+    delay_to_response = all_peaks_kinetics_data.iloc[0, 2] # gets the time of the first peak
+
+    # calculates the average kinetic values for all peaks in the given sweep 
+    all_peaks_kinetics_avg_data = pd.DataFrame(all_peaks_kinetics_data.mean(axis=0)).T
+    all_peaks_kinetics_avg_data['sweep #'] = all_peaks_kinetics_avg_data['sweep #'].astype(int) # makes sweep # an int
+    all_peaks_kinetics_avg_data.drop(['peak #', 'peak time (ms)'], axis=1, inplace=True) #drop peak # and peak time columns
+    all_peaks_kinetics_avg_data.insert(1, 'delay_to_response (ms)', delay_to_response)
+
+    # calculates the average kinetics values for the first three peaks in a sweep   
+    first3_kinetics_avg_data = pd.DataFrame(all_peaks_kinetics_data.iloc[:3].mean(axis=0)).T
+    first3_kinetics_avg_data['sweep #'] = first3_kinetics_avg_data['sweep #'].astype(int) # makes sweep # an int
+    first3_kinetics_avg_data.drop(['peak #', 'peak time (ms)'], axis=1, inplace=True) #drop peak # and peak time columns
+    first3_kinetics_avg_data.insert(1, 'delay_to_response (ms)', delay_to_response)
+
+    all_peaks_kinetics_df = pd.concat([all_peaks_kinetics_df, all_peaks_kinetics_data], ignore_index=True)
+    all_peaks_kinetics_avg_df = pd.concat([all_peaks_kinetics_avg_df, all_peaks_kinetics_avg_data], ignore_index=True)
+    first3_kinetics_avg_df = pd.concat([first3_kinetics_avg_df, first3_kinetics_avg_data], ignore_index=True)
+    
+# this df contains all kinetics values for all the peaks in all the sweeps
+data.all_peaks_kinetics_df = all_peaks_kinetics_df.set_index(['sweep #', 'peak #'], inplace=False)
+
+# this df contains avg kinetic values for all the peaks in all the sweeps
+data.all_peaks_kinetics_avg_df = all_peaks_kinetics_avg_df.set_index(['sweep #'], inplace=False)
+
+# this df contains avg kinetic values for the first three peaks in all the sweeps
+data.first3_kinetics_avg_df = first3_kinetics_avg_df.set_index(['sweep #'], inplace=False)
+
+# this df calculates the average of averages for the first three peaks in all the sweeps
+data.avg_first3_kinetics_avg_df = pd.DataFrame(first3_kinetics_avg_df.mean(axis=0)).T        
+data.avg_first3_kinetics_avg_df.insert(0, 'file_id', data.file_id)
+data.avg_first3_kinetics_avg_df.set_index(['file_id'], inplace=True)
+
+
+
+
+
+
+
+
+
+
+''' graph individual sweep'''
+sweep = 24
 
 trace = sweeps.iloc[window_start:, sweep].values
 thresh = 2.5 * trace.std()
