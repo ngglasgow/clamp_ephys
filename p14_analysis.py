31fd2601ed10659e35768c2a2254971b5a8486a5
Jane Huang <jhuang@pop-os.localdomain>
2020-06-01 18:13:19 -0400

    updated batch analysis files with kinetics code

--- p14_analysis.py
+++ p14_analysis.py
@@ -3,31 +3,38 @@ import pandas as pd
 import os
 
 '''####################### SET THE PROPER PATH YOU WANT ########################### '''
-paths = clamp_ephys.workflows.file_structure('local', 'Injected_GC_data/VC_pairs')
-tables = paths.tables
-figures = paths.figures
+project_path = clamp_ephys.workflows.file_structure('local', 'Injected_GC_data/VC_pairs')
+tables = project_path.tables
+figures = project_path.figures
 
 '''####################### SET PROPER PATH_TO_DATA_NOTES ########################## '''
-data_path = r"C:\Users\jhuang\Documents\phd_projects\Injected_GC_data\VC_pairs\tables\\p14_data_notes.csv"
+notes_path = os.path.join(tables, 'p14_data_notes.csv')
 
 
 ''' ################### SET/CHECK THESE PARAMETERS BEFORE RUNNING ################## '''
 lowpass_freq = 500  # Hz
 stim_time = 500     # ms
 post_stim = 250     # ms, amount of time after stimulus to look for max value
+baseline_start = 3000   # ms, time in the sweep to start taking the baseline
+baseline_end = 6000     # ms, time in the sweep at which baseline ends
 tp_start = 50        # ms, time of start of test pulse
 vm_jump = -5        # mV, test pulse voltage jump
 pre_tp = 11          # ms, amount of time before test pulse start to get baseline
 unit_scaler = -12   # unitless, scaler to get back to A, from pA
 amp_factor = 1000      # scaler for making plots in pA
 fs = 10             # kHz, the sampling frequency
+width = 3               # ms, the required width for classifying an event as a peak
+timepoint = 'p14'
 
 '''#################### THIS LOOP RUNS THE SINGLE CELL ANALYSIS #################### '''
 # p14_summary will hold the single line summary for each cell
 p14_summary = pd.DataFrame()
+p14_kinetics_summary = pd.DataFrame()
 
-for path in paths.p14_paths:
-    data = clamp_ephys.workflows.cell(path, fs=fs, path_to_data_notes=data_path, timepoint='p14', amp_factor=amp_factor)
+ncells = len(project_path.p14_paths)
+
+for file_number, cell in enumerate(project_path.p14_paths, 1):
+    data = clamp_ephys.workflows.cell(cell, fs=fs, path_to_data_notes=notes_path, timepoint=timepoint, amp_factor=amp_factor)
 
     data.get_raw_peaks(stim_time, post_stim)
     data.filter_traces(lowpass_freq)
@@ -47,5 +54,20 @@ for path in paths.p14_paths:
     data.save_mean_subtracted_trace(tables)
     # data.save_mean_peak_time(tables)
 
+    # kinetics analysis
+    data.get_peaks_widths(stim_time, width)
+    data.get_peaks_kinetics(stim_time)
+
+    data.save_all_peaks_kinetics(tables)
+    data.save_all_peaks_kinetics_avg(tables)
+    data.save_first3_kinetics_avg(tables)
+
+    p14_kinetics_summary = pd.concat([p14_kinetics_summary, data.avg_first3_kinetics_avg_df])
+
+    print(f'Finished analysis for {file_number} of {ncells} cells')
+
 summary_path = os.path.join(tables, 'p14_summary.csv')
+kinetics_summary_path = os.path.join(tables, 'p14_first3_kinetics_summary.csv')
+
 p14_summary.to_csv(summary_path, float_format='%8.4f', index=False)
+p14_kinetics_summary.to_csv(kinetics_summary_path, float_format='%8.4f', index=False)
--- p2_analysis.py
+++ p2_analysis.py
@@ -3,31 +3,39 @@ import pandas as pd
 import os
 
 '''####################### SET THE PROPER PATH YOU WANT ########################### '''
-paths = clamp_ephys.workflows.file_structure('local', 'Injected_GC_data/VC_pairs')
-tables = paths.tables
-figures = paths.figures
+project_path = clamp_ephys.workflows.file_structure('local', 'Injected_GC_data/VC_pairs')
+tables = project_path.tables
+figures = project_path.figures
+# cell_path = os.path.join(project_path, 'VC_pairs', 'data', 'p2', 'JH200313_c3_light100.ibw')
 
 '''####################### SET PROPER PATH_TO_DATA_NOTES ########################## '''
-data_path = r"C:\Users\jhuang\Documents\phd_projects\Injected_GC_data\VC_pairs\tables\\p2_data_notes.csv"
+notes_path = os.path.join(tables, 'p2_data_notes.csv')
 
 
 ''' ################### SET/CHECK THESE PARAMETERS BEFORE RUNNING ################## '''
-lowpass_freq = 500  # Hz
-stim_time = 500     # ms
-post_stim = 250     # ms, amount of time after stimulus to look for max value
-tp_start = 5        # ms, time of start of test pulse
-vm_jump = 10        # mV, test pulse voltage jump
-pre_tp = 3          # ms, amount of time before test pulse start to get baseline
-unit_scaler = -12   # unitless, scaler to get back to A, from pA
-amp_factor = 1      # scaler for making plots in pA
-fs = 25             # kHz, the sampling frequency
+lowpass_freq = 500      # Hz
+stim_time = 500         # ms
+post_stim = 250         # ms, amount of time after stimulus to look for max value
+baseline_start = 3000   # ms, time in the sweep to start taking the baseline
+baseline_end = 6000     # ms, time in the sweep at which baseline ends
+tp_start = 5            # ms, time of start of test pulse
+vm_jump = 10            # mV, test pulse voltage jump
+pre_tp = 3              # ms, amount of time before test pulse start to get baseline
+unit_scaler = -12       # unitless, scaler to get back to A, from pA
+amp_factor = 1          # scaler for making plots in pA
+fs = 25                 # kHz, the sampling frequency
+width = 3               # ms, the required width for classifying an event as a peak
+timepoint = 'p2'
 
 '''#################### THIS LOOP RUNS THE SINGLE CELL ANALYSIS #################### '''
 # p2_summary will hold the single line summary for each cell
 p2_summary = pd.DataFrame()
+p2_kinetics_summary = pd.DataFrame()
 
-for path in paths.p2_paths:
-    data = clamp_ephys.workflows.cell(path, fs=fs, path_to_data_notes=data_path, timepoint='p2', amp_factor=amp_factor)
+ncells = len(project_path.p2_paths)
+
+for file_number, cell in enumerate(project_path.p2_paths, 1):
+    data = clamp_ephys.workflows.cell(cell, fs=fs, path_to_data_notes=notes_path, timepoint=timepoint, amp_factor=amp_factor)
 
     data.get_raw_peaks(stim_time, post_stim)
     data.filter_traces(lowpass_freq)
@@ -47,5 +55,20 @@ for path in paths.p2_paths:
     data.save_mean_subtracted_trace(tables)
     # data.save_mean_peak_time(tables)
 
+    # kinetics analysis
+    data.get_peaks_widths(stim_time, width)
+    data.get_peaks_kinetics(stim_time)
+
+    data.save_all_peaks_kinetics(tables)
+    data.save_all_peaks_kinetics_avg(tables)
+    data.save_first3_kinetics_avg(tables)
+
+    p2_kinetics_summary = pd.concat([p2_kinetics_summary, data.avg_first3_kinetics_avg_df])
+
+    print(f'Finished analysis for {file_number} of {ncells} cells')
+
 summary_path = os.path.join(tables, 'p2_summary.csv')
+kinetics_summary_path = os.path.join(tables, 'p2_first3_kinetics_summary.csv')
+
 p2_summary.to_csv(summary_path, float_format='%8.4f', index=False)
+p2_kinetics_summary.to_csv(kinetics_summary_path, float_format='%8.4f', index=False)
